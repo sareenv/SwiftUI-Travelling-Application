aca86a4b06a96434a69bee59f5af2a369b62df7d
Vinayak <sareenv026@outlook.com>
2020-12-13 23:20:11 +0530

    Initial Commit

---
+++ TravelDiscoverSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelDiscoverSwiftUI
+//
+//  Created by DataBunker on 13/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
